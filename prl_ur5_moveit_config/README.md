d079d1b6551c1d7275a78d387e2e65b77e3f0cb0
Igor Kalevatykh <kalevatykh@gmail.com>
2021-04-12 01:17:07 +0200

    Add prl_ur5_moveit_config package

---
+++ prl_ur5_moveit_config/README.md
@@ -0,0 +1,2 @@
+# prl_ur5_moveit_config
+This package provides the configuration and launch files for using the PRL UR5 robot with the MoveIt Motion Planning Framework.
---
+++ prl_ur5_moveit_config/scripts/init_planning_scene.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+"""Script to initialize the planning scene."""
+
+import os
+import sys
+
+import geometry_msgs
+import moveit_commander
+import rospkg
+import rospy
+
+
+def run():
+    """Initialize the planning scene.
+    """
+    scene = moveit_commander.PlanningSceneInterface(synchronous=True)
+
+    # Remove all objects from the planning scene
+    scene.remove_world_object()
+
+    # Add the table model to the planning scene
+    r = rospkg.RosPack()
+    filename = os.path.join(
+        r.get_path('prl_ur5_description'), 'model', 'vention_table.stl')
+
+    pose = geometry_msgs.msg.PoseStamped()
+    pose.header.frame_id = 'stand_link'
+    pose.pose.orientation.w = 1.0
+
+    scene.add_mesh('table', pose, filename)
+
+
+if __name__ == '__main__':
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('init_planning_scene')
+    run()
