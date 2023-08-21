d2b428ca4bb2e52651c621f5a97369189916dc97
EtienneAr <>
2023-08-21 15:40:23 +0200

    Fix onrobot gripper controller position

--- prl_ur5_control/scripts/gripper_controller_launcher.sh
+++ prl_ur5_control/scripts/gripper_controller_launcher.sh
@@ -4,6 +4,8 @@
 # This script select which controller to run for the gripper (by reading ros param) and launch it.
 #  It expects the first argument to be 'left' or 'right' to determine the gripper,
 #  and all the other paramters will be forwarded to the controller (if needed).
+#
+# A script is necessary (rather than another launchfile) since it reads arguments in the parameter server.
 ########
 
 arg_nb=$#
@@ -11,11 +13,12 @@ arg_select_min=2 # The first argument is used to determine the side (left/right)
 arg_select_max=$arg_nb-2 # The last two argument of the script are __log and __name that is not needed for the gripper controller script
 arg_to_forward=${@:$arg_select_min:$arg_select_max} #Extract the arguyment to pass to the gripper controller script
 
+gripper_type=`rosparam get /setup/$1/gripper`
 gripper_controller=`rosparam get /setup/$1/gripper_controller`
 
 if [ "$gripper_controller" = "onrobot_rg" ]
 then
-    roslaunch onrobot_control onrobot_gripper.launch $arg_to_forward __ns:="$1_gripper"
+    roslaunch onrobot_control onrobot_gripper.launch gripper_type:=$gripper_type $arg_to_forward __ns:="$1_gripper"
 elif [ "$gripper_controller" = "fake" ]
 then
     rosrun prl_ur5_control fake_gripper_controller.py _joint:="$1_gripper_joint" __name:="gripper_controller" __ns:="$1_gripper"
